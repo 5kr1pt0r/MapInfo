6d1e3fcca0d85dcfa61c5d56f78d37ce74a8d374
5kr1pt0r <5kr1pt0r@protonmail.com>
2022-03-16 05:00:18 -0500

    MapInfo

---
+++ src/components/Card.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Card = ({ country }) => {
+  const { name, population, region, capital, flags, alpha3Code } = country;
+
+  return (
+    <Link
+      to={`/detail?country=${alpha3Code}`}
+      className="block overflow-hidden transform bg-white rounded-md cursor-pointer shadow-around dark:bg-dm-secondary hover:scale-105"
+    >
+      {" "}
+      <>
+        <div className="" style={{ height: "160px" }}>
+          <img
+            alt="country flag"
+            src={flags.png}
+            className="object-cover w-full h-full"
+          />
+        </div>
+        <div className="px-6 pt-6 pb-12">
+          <p className="mb-5 text-xl font-extrabold dark:text-white md:text-2xl">
+            {name}
+          </p>
+          <p className="text-lg">
+            <span className="font-bold dark:text-white">Population:</span>
+            <span className="text-gray-600 dark:text-gray-100">
+              {" "}
+              {population}
+            </span>{" "}
+          </p>
+          <p className="text-lg">
+            <span className="font-bold dark:text-white">Region:</span>{" "}
+            <span className="text-gray-600 dark:text-gray-100">{region}</span>
+          </p>
+          <p className="text-lg">
+            <span className="font-bold dark:text-white">Capital:</span>{" "}
+            <span className="text-gray-600 dark:text-gray-100">{capital}</span>
+          </p>
+        </div>
+      </>
+    </Link>
+  );
+};
+
+export default Card;
---
+++ src/layouts/Header.jsx
@@ -0,0 +1,36 @@
+import React, { useContext } from "react";
+import { ThemeContext } from "../ThemeContext";
+
+const Header = () => {
+  const { theme, setTheme } = useContext(ThemeContext);
+  const handleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+  return (
+    <header className="sticky top-0 z-50 shadow-md bg-lm-secondary dark:bg-dm-secondary">
+      {/* HEADER CONTAINER: */}
+      <div className="flex items-center justify-between px-6 py-6 mx-auto max-w-7xl">
+        <h1 className="text-2xl font-extrabold dark:text-white">
+          Striptor Map
+        </h1>
+        <div>
+          <i
+            className={`fa fa-${
+              theme === "dark" ? "sun" : "moon"
+            } dark:text-white`}
+            id="theme-icon"
+          ></i>
+          <span
+            className="ml-2 cursor-pointer text-lm-text dark:text-dm-text "
+            id="theme-toggle"
+            onClick={handleTheme}
+          >
+            {theme === "dark" ? "Light Mode" : "Dark Mode"}
+          </span>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
